76867d0b6fa3347f3481583ce5d349ae86dcc842
Ben-Gendler <57101218+Ben-Gendler@users.noreply.github.com>
2019-12-10 17:37:29 +0200

    Important Update
    Created a new collection<function> Arraylist based 
    added implementation of all collection relevant methods
    Added try and catch for each one (when adequate)

--- Functions_GUI.java
+++ Functions_GUI.java
@@ -1,111 +1,170 @@
 package myMath;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+
 public class Functions_GUI implements functions {
+	Collection<function> collection = new ArrayList<function>();
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return collection.size();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		if (collection.size() ==0)
+			return true;
 		return false;
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
+		if(collection.contains(o))
+			return true;
 		return false;
+
 	}
 
 	@Override
 	public Iterator<function> iterator() {
-		// TODO Auto-generated method stub
+		//		this.collection.iterator().hasNext();
 		return null;
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			collection.toArray();
+		}
+		catch(NullPointerException ex) {
+			throw new NullPointerException("An NullPointerException occured, please re-check");
+		}
+		catch(  ArrayStoreException  ex ) {
+			throw new ArrayStoreException("An ArrayStoreException occured, please re-check");
+		}
+
+		return collection.toArray();
 	}
 
 	@Override
 	public <T> T[] toArray(T[] a) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			collection.toArray(a);
+		}
+		catch(NullPointerException ex) {
+			throw new NullPointerException("An NullPointerException occured, please re-check");
+		}
+		catch(  ArrayStoreException  ex ) {
+			throw new ArrayStoreException("An ArrayStoreException occured, please re-check");
+		}
+		return collection.toArray(a);
 	}
 
 	@Override
 	public boolean add(function e) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			collection.add(e);
+		}
+		catch(ClassCastException | NullPointerException | UnsupportedOperationException ex ) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			collection.remove(o);
+		}
+		catch(  NullPointerException | ClassCastException  ex ) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			collection.containsAll(c);
+		}
+		catch( UnsupportedOperationException |NullPointerException | IllegalArgumentException |IllegalStateException ex ) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends function> c) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			collection.addAll(c);
+		}
+		catch( UnsupportedOperationException |NullPointerException | IllegalArgumentException  ex ) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			collection.removeAll(c);
+		}
+		catch( UnsupportedOperationException |NullPointerException | IllegalArgumentException  ex ) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
 	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			collection.retainAll(c);
+		}
+		catch( UnsupportedOperationException |NullPointerException | ClassCastException  ex ) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		try {
+			collection.clear();
+		}
+		catch( UnsupportedOperationException |NullPointerException | ClassCastException  ex ) {
+			System.out.println("An error occured. please check");
+		}
+
 	}
 
 	@Override
 	public void initFromFile(String file) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void saveToFile(String file) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void drawFunctions(String json_file) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
