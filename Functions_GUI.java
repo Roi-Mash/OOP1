ea760da8ee6b47f397b4dbccdddd83c07ca25427
talbg <57101649+talbg@users.noreply.github.com>
2019-12-11 22:27:46 +0200

    Update Functions_GUI.java
    update initFromFile

--- Functions_GUI.java
+++ Functions_GUI.java
@@ -222,8 +222,61 @@ public class Functions_GUI implements functions {
 
 	@Override
 	public void initFromFile(String file) throws IOException {
-	
-		
+		try{
+
+			// Open the file that is the first 
+			// command line parameter
+
+			//checks where the path is going ?????????????????????
+			System.out.println(System.getProperty("user.dir"));
+
+			//
+			FileInputStream fstream = new FileInputStream(file);
+
+
+			// Get the object of DataInputStream
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+
+
+			//Read File Line By Line
+			this.collection.clear();
+
+			while ((strLine = br.readLine()) != null) 	{
+
+				if(lineChecker(strLine)){
+
+
+					if(strLine.contains("(")) {
+						//create new complexFunction from string
+						//add to collection
+
+					}
+					else if(strLine.substring(1).contains("+")||strLine.substring(1).contains("-")) {
+						Polynom strLineP = new Polynom(strLine);
+						this.add(strLineP);
+					}
+				}
+				else {
+					throw new IOException("Wrong string in file");
+				}
+			}
+
+		}
+		catch(Exception e){
+			}
+		}
+
+
+
+
+
+
+
+		private boolean lineChecker(String strLine) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
