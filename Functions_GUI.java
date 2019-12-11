7157468e575ce5e5553c615dd5eb29cdc956313b
Ben-Gendler <57101218+Ben-Gendler@users.noreply.github.com>
2019-12-11 12:07:12 +0200

    Update Functions_GUI.java

--- Functions_GUI.java
+++ Functions_GUI.java
@@ -4,47 +4,56 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-
-
+import myMath.Range;
 public class Functions_GUI implements functions {
-	Collection<function> collection = new ArrayList<function>();
+
+	Collection<function> collection = new ArrayList<function>(); //Configuring the ArrayList
+	//params
 	int width,height,resolution;
 	double[] RangeX = new double[2];
 	double[] RangeY = new double[2];
 
 
 	@Override
-	public int size() {//v
+	public int size() {
 		return collection.size();
 	}
 
 	@Override
-	public boolean isEmpty() {	//v
+	public boolean isEmpty() {
 		return collection.isEmpty();
 	}
-	
+
 	@Override
-	public boolean contains(Object o) {//v
+	public boolean contains(Object o) {
+		if(collection.contains(o))
+			return true;
+		return false;
+
+	}
+
+	@Override
+	public Iterator<function> iterator() {
+		return collection.iterator();
+	}
+
+	@Override
+	public Object[] toArray() {
 		try {
-			collection.contains(o);
+			collection.toArray();
 		}
-		catch(ClassCastException |NullPointerException ex) {
-			return false;
+		catch(NullPointerException ex) {
+			throw new NullPointerException("An NullPointerException occured, please re-check");
 		}
-		return true;
+		catch(  ArrayStoreException  ex ) {
+			throw new ArrayStoreException("An ArrayStoreException occured, please re-check");
 		}
-		@Override
-	public Iterator<function> iterator() {//v
-	return collection.iterator();
+
+		return collection.toArray();
 	}
 
 	@Override
-	public Object[] toArray() {//v
-	return collection.toArray();
-	}
-
-	@Override
-	public <T> T[] toArray(T[] a) {//v
+	public <T> T[] toArray(T[] a) {
 		try {
 			collection.toArray(a);
 		}
@@ -58,55 +67,55 @@ public class Functions_GUI implements functions {
 	}
 
 	@Override
-	public boolean add(function e) {//v
+	public boolean add(function e) {
 		try {
 			collection.add(e);
 		}
-		catch(ClassCastException | NullPointerException | UnsupportedOperationException|IllegalArgumentException | IllegalStateException ex ) {
+		catch(ClassCastException | NullPointerException | UnsupportedOperationException ex ) {
 			return false;
 		}
 		return true;
 	}
 
 	@Override
-	public boolean remove(Object o) {//v
+	public boolean remove(Object o) {
 		try {
 			collection.remove(o);
 		}
-		catch(  NullPointerException | ClassCastException|UnsupportedOperationException  ex ) {
+		catch(  NullPointerException | ClassCastException  ex ) {
 			return false;
 		}
 		return true;
 	}
 
 	@Override
-	public boolean containsAll(Collection<?> c) {//v
+	public boolean containsAll(Collection<?> c) {
 		try {
 			collection.containsAll(c);
 		}
-		catch( NullPointerException |ClassCastException ex ) {
+		catch( UnsupportedOperationException |NullPointerException | IllegalArgumentException |IllegalStateException ex ) {
 			return false;
 		}
 		return true;
 	}
 
 	@Override
-	public boolean addAll(Collection<? extends function> c) {//v
+	public boolean addAll(Collection<? extends function> c) {
 		try {
 			collection.addAll(c);
 		}
-		catch( UnsupportedOperationException |NullPointerException | IllegalArgumentException |IllegalStateException|ClassCastException ex ) {
+		catch( UnsupportedOperationException |NullPointerException | IllegalArgumentException  ex ) {
 			return false;
 		}
 		return true;
 	}
 
 	@Override
-	public boolean removeAll(Collection<?> c) {//v
+	public boolean removeAll(Collection<?> c) {
 		try {
 			collection.removeAll(c);
 		}
-		catch( UnsupportedOperationException |NullPointerException | ClassCastException  ex ) {
+		catch( UnsupportedOperationException |NullPointerException | IllegalArgumentException  ex ) {
 			return false;
 		}
 
@@ -114,7 +123,7 @@ public class Functions_GUI implements functions {
 	}
 
 	@Override
-	public boolean retainAll(Collection<?> c) {//v
+	public boolean retainAll(Collection<?> c) {
 		try {
 			collection.retainAll(c);
 		}
@@ -151,13 +160,18 @@ public class Functions_GUI implements functions {
 	@Override
 	public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
 		SetParams(width,height,rx,ry,resolution);
-		while(collection.iterator().hasNext()) {
-			//codegoeshere->
+		Iterator<function> itr = collection.iterator();
+		while(itr.hasNext()) {
+			DrawFunction(itr.next());			
 			collection.iterator().next();
 		}
+
 	}
-	
-		public void SetParams(int width, int height, Range rx, Range ry, int resolution) {
+	public void DrawFunction(function a) {
+
+	}
+
+	public void SetParams(int width, int height, Range rx, Range ry, int resolution) {
 		this.width = width;
 		this.height = height;
 		RangeX[0] =rx.get_min();
@@ -168,7 +182,6 @@ public class Functions_GUI implements functions {
 
 
 	}
-
 	@Override
 	public void drawFunctions(String json_file) {
 		// TODO Auto-generated method stub
